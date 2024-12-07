3b042f69e793af94b4d5d6bfa362234f199de093
rodiond26 <rodiond26@gmail.com>
2024-12-07 21:43:12 +0500

    HW02 поправил замечания ПРа

--- L03-generics/src/main/java/ru/otus/homework/CustomerService.java
+++ L03-generics/src/main/java/ru/otus/homework/CustomerService.java
@@ -7,24 +7,20 @@ import java.util.TreeMap;
 
 public class CustomerService {
 
-    private Comparator<Customer> customerComparator = Comparator.comparingLong(Customer::getScores);
+    private final Comparator<Customer> customerComparator = Comparator.comparingLong(Customer::getScores);
     private final NavigableMap<Customer, String> customerData = new TreeMap<>(customerComparator);
 
     public Map.Entry<Customer, String> getSmallest() {
-        return customerData.entrySet()
-                .stream()
-                .findFirst()
-                .map(this::copy)
-                .orElse(null);
+        return customerData.firstEntry() == null
+                ? null
+                : Map.entry(Customer.copy(customerData.firstEntry().getKey()), customerData.firstEntry().getValue());
     }
 
     public Map.Entry<Customer, String> getNext(Customer customer) {
-        return customerData.entrySet()
-                .stream()
-                .filter(entry -> entry.getKey().getScores() > customer.getScores())
-                .findFirst()
-                .map(this::copy)
-                .orElse(null);
+        Map.Entry<Customer, String> higherEntry = customerData.higherEntry(customer);
+        return higherEntry == null
+                ? null
+                : Map.entry(Customer.copy(higherEntry.getKey()), higherEntry.getValue());
     }
 
     public void add(Customer customer, String data) {
